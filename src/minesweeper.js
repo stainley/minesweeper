00c65f43eb682828469f889b83985fa9a8858999
Stainley Lebron <saiyamans@gmail.com>
2018-04-28 22:06:56 -0400

    Formatting board method

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+const blankLine = '   |   |   ';
+
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+const guessLine = '1  |   |   ';
+const bombLine = '   | B |   ';
+
+console.log('This is what a board with a guess and bomb on it would look like:');
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
